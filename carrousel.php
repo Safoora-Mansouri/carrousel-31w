ebe484d4c603f37c87c3c386f1005500eb07f0c9
safoora <safoura.mansouri@gmail.com>
2023-03-19 21:53:43 -0400

    modifier carrousel

--- carrousel.php
+++ carrousel.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
 * Extension carrousel permet d'afficher dans une boîte modale 
@@ -34,19 +35,15 @@ add_action('wp_enqueue_scripts', 'carrousel_enqueue');
 //generer le code HTML//
 function creation_carrousel()
 {
-return '<button class="bouton__ouvrir">Ouvrir</button>
+return '
     <div class="carrousel">
     <button class="bouton__x">X</button>
     <figure class="carrousel__figure"></figure>
     <div class="controls">
-        <button class="button__next"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
-  <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
-</svg>
-      </button>
-        <button class="button__previous"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left" viewBox="0 0 16 16">
-  <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
-</svg>
-      </button>
+        <button class="bouton__suivant"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/></svg></button>
+        <button class="bouton__precedent"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
+        <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/></svg></button>
     </div>
     <form class="carrousel__form"></form>
     </div>';
